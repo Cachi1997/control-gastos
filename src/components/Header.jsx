abd10f2177cce7b38f027264445a83b099c90d60
Carlos Galiano <cachigaliano1997@gmail.com>
2023-02-21 18:32:12 -0300

    vista de modal añadida

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import { ControlPresupuesto } from './ControlPresupuesto'
 import { NuevoPresupuesto } from './NuevoPresupuesto'
 
 export const Header = ({presupuesto, setPresupuesto, isValidPresupuesto, setIsValidPresupuesto}) => {
+
   return (
     <header>
         <h1>Planificador de Gastos</h1>
